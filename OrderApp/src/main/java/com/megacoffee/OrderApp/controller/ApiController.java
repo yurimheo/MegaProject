4f16698d370a69eb3ce90b62005e7375e3bef02a
yurimheo <yurim1325@gmail.com>
2023-11-20 16:04:45 +0900

    add Controller basic

---
+++ OrderApp/src/main/java/com/megacoffee/OrderApp/controller/ApiController.java
@@ -0,0 +1,10 @@
+package com.megacoffee.OrderApp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ApiController {
+    // 기능 구현
+
+}
---
+++ OrderApp/src/main/java/com/megacoffee/OrderApp/controller/ViewController.java
@@ -0,0 +1,20 @@
+package com.megacoffee.OrderApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    // 창 이동 - 화면에 보여지는 GetMapping, requestMapping,,,, 맵핑류
+
+    // Localhost:8080 + /login : 로그인 화면으로 이동
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/login";
+    }
+    @GetMapping("/login")
+    public String main(){
+        return "loginForm";
+    }
+}
